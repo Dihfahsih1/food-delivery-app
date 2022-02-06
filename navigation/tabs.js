19e4b9340ef41673c3c698c2e4eb9d99d9f593e5
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-06 23:52:50 +0300

    add more icons on the bottom screen

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -36,6 +36,63 @@ const Tabs = () => {
           )
         }}
       />
+
+      <Tab.Screen
+        name="search"
+        component={Home}
+        options={{
+          tabBarIcon:({focused})=>(
+          <Image 
+            source={icons.search}
+            resizeMode = "contain"
+            style={{
+              width:25,
+              height:25,
+              tintColor: focused ? COLORS.primary : COLORS.secondary
+            }}
+          
+          />
+          )
+        }}
+      />
+
+      <Tab.Screen
+        name="Like"
+        component={Home}
+        options={{
+          tabBarIcon:({focused})=>(
+          <Image 
+            source={icons.like}
+            resizeMode = "contain"
+            style={{
+              width:25,
+              height:25,
+              tintColor: focused ? COLORS.primary : COLORS.secondary
+            }}
+          
+          />
+          )
+        }}
+      />
+
+<Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          tabBarIcon:({focused})=>(
+          <Image 
+            source={icons.user}
+            resizeMode = "contain"
+            style={{
+              width:25,
+              height:25,
+              tintColor: focused ? COLORS.primary : COLORS.secondary
+            }}
+          
+          />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
