1b14a7f177cace605ab6d04148d191c474549ab4
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-07 11:03:30 +0300

    add custom button

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -38,7 +38,12 @@ const Tabs = () => {
             }}
           
           />
-          )
+          ),
+            tabBarButton:(props) =>(
+              <TabBarCustomButton
+              {...props}
+              />
+            )
         }}
       />
 
@@ -57,6 +62,11 @@ const Tabs = () => {
             }}
           
           />
+          ),
+          tabBarButton:(props) =>(
+            <TabBarCustomButton
+            {...props}
+            />
           )
         }}
       />
@@ -76,28 +86,38 @@ const Tabs = () => {
             }}
           
           />
+          ),
+          tabBarButton:(props) =>(
+            <TabBarCustomButton
+            {...props}
+            />
           )
         }}
       />
 
-<Tab.Screen
-        name="User"
-        component={Home}
-        options={{
-          tabBarIcon:({focused})=>(
-          <Image 
-            source={icons.user}
-            resizeMode = "contain"
-            style={{
-              width:25,
-              height:25,
-              tintColor: focused ? COLORS.primary : COLORS.secondary
-            }}
-          
-          />
-          )
-        }}
-      />
+      <Tab.Screen
+          name="User"
+              component={Home}
+              options={{
+                tabBarIcon:({focused})=>(
+                <Image 
+                  source={icons.user}
+                  resizeMode = "contain"
+                  style={{
+                    width:25,
+                    height:25,
+                    tintColor: focused ? COLORS.primary : COLORS.secondary
+                  }}
+                
+                />
+                ),
+                tabBarButton:(props) =>(
+                  <TabBarCustomButton
+                  {...props}
+                  />
+                )
+              }}
+            />
     </Tab.Navigator>
   )
 }
