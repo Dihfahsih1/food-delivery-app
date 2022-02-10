f0d0e2c0e4a54be2c3351a7711efb97bcee49446
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-10 21:17:02 +0300

    add back arrow on the restaurant page

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import {View,SafeAreaView,Image, TouchableOpacity, StyleSheet,FlatList, Text} from 'react-native';
-const Restaurant = () => {
+import {View,SafeAreaView,Image, TouchableOpacity, StyleSheet,FlatList, Text, Animated} from 'react-native';
+
+import {COLORS, icons, images, SIZES,FONTS} from '../constants'
+const Restaurant = ({route, navigation}) => {
+
+  const[restaurant, setRestaurant] = React.setState(null);
+  const[currentLocation, setcurrentLocation] = React.setState(null);
+
+  React.useEffect(() =>{
+    let{item, currentLocation} = route.params
+
+    setRestaurant(item)
+    setcurrentLocation(currentLocation)
+  })
+
+function renderHeader(){
   return (
-    <View>
-      <Text>Restaurant</Text>
+    <View style={{flexDirection:'row'}}>
+      <TouchableOpacity
+        style={{width:50, paddingLeft:SIZES.padding * 1, justifyContent:'center'}}
+        
+        onPress={() => navigation.goBack()}>
+
+        <Image
+          source={icons.back}
+          resizeMode='contain'
+          style={{width:30, height:30}}/>
+      </TouchableOpacity>
 
     </View>
   )
+}
+
+  return (
+    <SafeAreaView>
+      {renderHeader()}
+    </SafeAreaView>
+  )
 
 };
+const styles = styles.create({
+  container: {
+    flex:1,
+    backgroundColor=COLORS.lightGray2
+
+  }
+
+})
 
 export default Restaurant;
