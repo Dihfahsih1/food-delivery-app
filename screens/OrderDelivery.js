435f06108db3966581aded7fd021dbe46ddaa3e9
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-16 13:28:53 +0300

    add map to order delivery screen

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import {View,SafeAreaView,Image, TouchableOpacity, StyleSheet,FlatList, Text, Animated} from 'react-native';
+import MapView ,{PROVIDER_GOOGLE, Marker} from 'react-native-maps'
 const OrderDelivery = () => {
+  function renderMap(){
+    return(
+      <View style={{flex: 1}}>
+        <MapView style={{flex: 1}}>
+
+        </MapView>
+        
+      </View>
+
+    )
+  }
   return (
-    <View>
-      <Text>OrderDelivery</Text>
+    <View style={{ flex: 1}}>
+      {renderMap()}
+      
 
     </View>
   )
