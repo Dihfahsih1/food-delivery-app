db99c8aeb53a04ac8d90667e2b5f105dc6cb0354
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-16 10:08:31 +0300

    show counter value

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -51,8 +51,10 @@ function getOrderQty(menuId){
   let orderItem = orderItems.filter(a => a.menuId == menuId)
 
   if(orderItem.length > 0){
+    return orderItem[0].qty
 
   }
+  return 0
 }
 function renderHeader(){
   return (
