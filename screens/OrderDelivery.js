456f9308c858046d8165d5ef166d6ba60017574e
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-17 16:18:15 +0300

    ignore the maps file

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View,SafeAreaView,Image, TouchableOpacity, StyleSheet,FlatList, Text, Animated} from 'react-native';
 import MapView ,{PROVIDER_GOOGLE, Marker} from 'react-native-maps'
+import {COLORS, icons, images, SIZES,FONTS, GOOGLE_API_KEY} from '../constants'
 const OrderDelivery = () => {
   function renderMap(){
     return(
